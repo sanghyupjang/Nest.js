941d8a9e169c5072769029e525a55d115a93c4c7
Sanghyup <sanghyeob93@naver.com>
2022-04-20 11:42:02 +0900

    push

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,10 @@
 import Seo from "../components/Seo";
 
-export default function Potato() {
+export default function Home() {
   return (
     <div>
-      <Seo title="About" />
-      <h1>About</h1>
+      <Seo title="Home" />
+      <h1 className="active">Hello</h1>
     </div>
   );
 }
